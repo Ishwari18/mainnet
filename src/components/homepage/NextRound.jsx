b64f3160f9184fb632e61b9bdde87f20608cf208
Ishwari18 <ishwarirele18@.com>
2023-05-30 23:09:31 +0530

    timer

--- src/components/homepage/NextRound.jsx
+++ src/components/homepage/NextRound.jsx
@@ -14,294 +14,298 @@ export default function NextRound() {
   const [timeLeft, setTimeLeft] = useState(0);
 
   const stakingcontractAddress = "0x2C4E19b1B46857caa94CF4d219A82dc38C865220";
-const stakingcontractABI = [
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "_minimumDurationInDays",
-				"type": "uint256"
-			},
-			{
-				"internalType": "address",
-				"name": "_batman",
-				"type": "address"
-			}
-		],
-		"stateMutability": "nonpayable",
-		"type": "constructor"
-	},
-	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "winner",
-				"type": "address"
-			},
-			{
-				"indexed": false,
-				"internalType": "uint256",
-				"name": "amount",
-				"type": "uint256"
-			}
-		],
-		"name": "JackpotWon",
-		"type": "event"
-	},
-	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": false,
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			},
-			{
-				"indexed": false,
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"name": "Received",
-		"type": "event"
-	},
-	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "staker",
-				"type": "address"
-			},
-			{
-				"indexed": false,
-				"internalType": "uint256",
-				"name": "amount",
-				"type": "uint256"
-			}
-		],
-		"name": "Staked",
-		"type": "event"
-	},
-	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "staker",
-				"type": "address"
-			},
-			{
-				"indexed": false,
-				"internalType": "uint256",
-				"name": "amount",
-				"type": "uint256"
-			}
-		],
-		"name": "Unstaked",
-		"type": "event"
-	},
-	{
-		"inputs": [],
-		"name": "activateRaffle",
-		"outputs": [],
-		"stateMutability": "payable",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "batman",
-		"outputs": [
-			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "getJackpotBalance",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "getStakerAddresses",
-		"outputs": [
-			{
-				"internalType": "address[]",
-				"name": "",
-				"type": "address[]"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "lastActivationTime",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "minimumDuration",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "owner",
-		"outputs": [
-			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "amount",
-				"type": "uint256"
-			}
-		],
-		"name": "stake",
-		"outputs": [],
-		"stateMutability": "payable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"name": "stakers",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "stakedAmount",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint256",
-				"name": "timestamp",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "totalTokensStaked",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "unstake",
-		"outputs": [],
-		"stateMutability": "payable",
-		"type": "function"
-	},
-	{
-		"stateMutability": "payable",
-		"type": "receive"
-	}
-]
+  const stakingcontractABI = [
+    {
+      inputs: [
+        {
+          internalType: "uint256",
+          name: "_minimumDurationInDays",
+          type: "uint256",
+        },
+        {
+          internalType: "address",
+          name: "_batman",
+          type: "address",
+        },
+      ],
+      stateMutability: "nonpayable",
+      type: "constructor",
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: "address",
+          name: "winner",
+          type: "address",
+        },
+        {
+          indexed: false,
+          internalType: "uint256",
+          name: "amount",
+          type: "uint256",
+        },
+      ],
+      name: "JackpotWon",
+      type: "event",
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: false,
+          internalType: "address",
+          name: "",
+          type: "address",
+        },
+        {
+          indexed: false,
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      name: "Received",
+      type: "event",
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: "address",
+          name: "staker",
+          type: "address",
+        },
+        {
+          indexed: false,
+          internalType: "uint256",
+          name: "amount",
+          type: "uint256",
+        },
+      ],
+      name: "Staked",
+      type: "event",
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: "address",
+          name: "staker",
+          type: "address",
+        },
+        {
+          indexed: false,
+          internalType: "uint256",
+          name: "amount",
+          type: "uint256",
+        },
+      ],
+      name: "Unstaked",
+      type: "event",
+    },
+    {
+      inputs: [],
+      name: "activateRaffle",
+      outputs: [],
+      stateMutability: "payable",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "batman",
+      outputs: [
+        {
+          internalType: "address",
+          name: "",
+          type: "address",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "getJackpotBalance",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "getStakerAddresses",
+      outputs: [
+        {
+          internalType: "address[]",
+          name: "",
+          type: "address[]",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "lastActivationTime",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "minimumDuration",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "owner",
+      outputs: [
+        {
+          internalType: "address",
+          name: "",
+          type: "address",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "uint256",
+          name: "amount",
+          type: "uint256",
+        },
+      ],
+      name: "stake",
+      outputs: [],
+      stateMutability: "payable",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "address",
+          name: "",
+          type: "address",
+        },
+      ],
+      name: "stakers",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "stakedAmount",
+          type: "uint256",
+        },
+        {
+          internalType: "uint256",
+          name: "timestamp",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "totalTokensStaked",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "unstake",
+      outputs: [],
+      stateMutability: "payable",
+      type: "function",
+    },
+    {
+      stateMutability: "payable",
+      type: "receive",
+    },
+  ];
 
   useEffect(() => {
-	const checkJackpotWon = async () => {
-		try {
-		  const contract = new ethers.Contract(stakingcontractAddress, stakingcontractABI, provider);
-		  const filter = contract.filters.JackpotWon(); // Assuming the event name is JackpotWon
-	
-		  // Check if the JackpotWon event is triggered in the contract
-		  const events = await contract.queryFilter(filter);
-	
-		  if (events.length > 0) {
-			// If the event is triggered, start the timer
-			fetchTimeLeft();
-		  }
-		} catch (error) {
-		  console.error('Error checking JackpotWon event:', error);
-		}
-	  };
-	
-	  checkJackpotWon();
+    const checkJackpotWon = async () => {
+      try {
+        const contract = new ethers.Contract(
+          stakingcontractAddress,
+          stakingcontractABI,
+          provider
+        );
+        const filter = contract.filters.JackpotWon(); // Assuming the event name is JackpotWon
+        // Check if the JackpotWon event is triggered in the contract
+        const events = await contract.queryFilter(filter);
+
+        if (events.length > 0) {
+          // If the event is triggered, start the timer
+          fetchTimeLeft();
+        }
+      } catch (error) {
+        console.error("Error checking JackpotWon event:", error);
+      }
+    };
+
+    checkJackpotWon();
     let isMounted = true;
 
     const fetchTimeLeft = async () => {
       try {
-        const contract = new ethers.Contract(stakingcontractAddress, stakingcontractABI, provider);
+        const contract = new ethers.Contract(
+          stakingcontractAddress,
+          stakingcontractABI,
+          provider
+        );
         const lastActivationTime = await contract.lastActivationTime();
-		//console.log(lastActivationTime);
+        //console.log(lastActivationTime);
         const currentTime = Math.floor(Date.now() / 1000);
-        const countdown = 60 * 60 *24 * 7; //7 days 
-        const timeSinceLastActivation = currentTime - lastActivationTime.toNumber();
+        const countdown = 60 * 60 * 24 * 7; //7 days
+        const timeSinceLastActivation =
+          currentTime - lastActivationTime.toNumber();
         const remainingTime = countdown - (timeSinceLastActivation % countdown);
 
         if (isMounted) {
           setTimeLeft(remainingTime);
         }
       } catch (error) {
-        console.error('Error fetching time left:', error);
+        console.error("Error fetching time left:", error);
       }
     };
-
-    
 
     return () => {
       isMounted = false;
     };
   }, [stakingcontractAddress, stakingcontractABI]);
-
-  
 
   const formatTime = (time) => {
     const days = Math.floor(time / (24 * 60 * 60));
@@ -312,7 +316,6 @@ const stakingcontractABI = [
     return `${days}d ${hours}h ${minutes}m ${seconds}s`;
   };
 
-
   return (
     <div className={styles.NextRound}>
       <h1 className={styles.h1}>Next Round</h1>
@@ -320,9 +323,7 @@ const stakingcontractABI = [
         <span>5 Days 1 : </span>
         <span>59</span>
       </div> */}
-      <div>
-      Time left until next raffle: {formatTime(timeLeft)}
-    </div>
+      <div>Time left until next raffle: {formatTime(timeLeft)}</div>
     </div>
   );
 }
